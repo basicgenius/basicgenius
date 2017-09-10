fdc5b090a5213ff9141df2258a842953ef24bae8
Locke Noah J <NLocke@uwhealth.org>
2017-09-10 00:43:16 -0500

    added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
+	siteMetadata: {
+	title: `Gatsby Default Starter`,
+	},
+  	plugins: [`gatsby-plugin-react-helmet`],
+  	plugins: [
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-106150903-1',
+		},
+	  },
+	]
 }
